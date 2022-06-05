5bc21b0c47d34ebe8a2b0f7e8342486d8cde1808
grusha4669 <grusha4669@gmail.com>
2022-06-06 02:21:10 +0300

    m

---
+++ app/src/main/java/ru/pashaginas/loftcoin/activity/SActivity.java
@@ -0,0 +1,15 @@
+package ru.pashaginas.loftcoin.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ru.pashaginas.loftcoin.R;
+
+public class SActivity extends AppCompatActivity {
+
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_s);
+   }
+}
